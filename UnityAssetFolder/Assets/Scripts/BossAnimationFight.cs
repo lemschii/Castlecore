d849c79ce0c2b5c21acf9337af5bef73eb09d64f
hochstegaa <felix.hochsteger@gmx.net>
2024-05-22 08:25:01 +0200

    Assetfolder 22.05

--- UnityAssetFolder/Assets/Scripts/BossAnimationFight.cs
+++ UnityAssetFolder/Assets/Scripts/BossAnimationFight.cs
@@ -16,8 +16,7 @@ public class BossAnimationFight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
--- UnityAssetFolder/Assets/Scripts/Bossmovement.cs
+++ UnityAssetFolder/Assets/Scripts/Bossmovement.cs
@@ -7,31 +7,70 @@ public class Bossmovement : MonoBehaviour
     public float moveSpeed = 5f;  // Speed of movement towards the player
     public float burstDuration = 2f;  // Duration of each movement burst
     public float waitDuration = 5f;  // Duration to wait between bursts
+    public Animator animator;
+    public Transform slimeTransform;
 
     private Transform player;  // Reference to the player's transform
     private Vector3 moveDirection;  // Direction to move towards the player
     private float burstTimer;  // Timer for measuring burst duration
     private float waitTimer;  // Timer for measuring wait duration
+    
+    public float attackRange = 10f;
+    public int attackDamage = 50;
+    public float attackRate = 2f;
+    private float nextAttacktime = 0f;
+    
+    public int maxHealth;
+    private int currentHealth;
+    
+    public LayerMask enemyLayers;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         burstTimer = burstDuration;  // Start by moving immediately
         waitTimer = waitDuration;  // Start waiting after the first burst
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
     }
 
     void Update()
     {
-        // Calculate direction towards the player
-        moveDirection = (player.position - transform.position).normalized;
+        if (Time.time >= nextAttacktime)
+        { 
+            Attack();
+            nextAttacktime = Time.time + 1f / attackRate;
+        }
+    }
 
-        if (burstTimer > 0f)
+    private void Attack()
+    {
+        Debug.Log("rawr xDDDD Lollll :> Blehhh :PPPP >.<");
+        //detect if player is in dash range (SCHAUN OB DIE RANGE PASST, WEILS JO VOM TRANSFORM AUSGEHT) 
+        Collider2D hitEnemies = Physics2D.OverlapCircle(transform.position, 100f, enemyLayers);
+        
+        if (burstTimer > 0f && hitEnemies)
         {
+            Debug.Log("Meiow");
+            // Calculate direction towards the player
+            moveDirection = (player.position - transform.position).normalized;
+            
             // Move towards the player during the burst
             float moveDistance = moveSpeed * Time.deltaTime;
             transform.position += moveDirection * moveDistance;
-
+            
             burstTimer -= Time.deltaTime;  // Decrease burst timer
+            
+            Collider2D[] hitplayer = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayers);
+            //Damage Calculation
+            foreach (Collider2D enemy in hitplayer)
+            {
+                animator.SetTrigger("IsHittingPlayer");
+                //Debug.Log("Hit  " + enemy.name);
+                enemy.GetComponent<PlayerCombat>().TakeDamage(attackDamage);
+            }
+            //detetct if player is in damage range
+            
         }
         else
         {
